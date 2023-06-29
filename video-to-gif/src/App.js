03afbf050543d97a26db82d658a9863572f932d1
Willian Lima <willian.slima@mercadolivre.com>
2023-06-29 15:45:28 -0300

    upload de video ok

--- video-to-gif/src/App.js
+++ video-to-gif/src/App.js
@@ -1,22 +1,37 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [videoUploaded, setVideoUploaded] = useState(false);
+  const [videoSrc, setVideoSrc] = useState('');
+
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+    const videoUrl = URL.createObjectURL(file);
+    setVideoSrc(videoUrl);
+    setVideoUploaded(true);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Converter vídeo em .GIF
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input
+          type="file"
+          accept="video/*"
+          onChange={handleFileUpload}
+        />
+        {videoUploaded && (
+          <div className="video-container">
+            <video id="uploaded-video" controls>
+              <source src={videoSrc} type="video/mp4" />
+            </video>
+          </div>
+        )}
       </header>
     </div>
   );
