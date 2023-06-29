070f450396475c1ab9fb2808cb7f7d25823aef2a
Willian Lima <willian.slima@mercadolivre.com>
2023-06-29 17:13:46 -0300

    Gerando arquivo

--- video-to-gif/src/App.js
+++ video-to-gif/src/App.js
@@ -1,24 +1,44 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import gifshot from 'gifshot';
 
 function App() {
   const [videoUploaded, setVideoUploaded] = useState(false);
-  const [videoSrc, setVideoSrc] = useState('');
-  const [showAlert, setShowAlert] = useState(false);
+  const [videoFile, setVideoFile] = useState(null);
+  const [gifUrl, setGifUrl] = useState('');
 
   const handleFileUpload = (event) => {
     const file = event.target.files[0];
-    const videoUrl = URL.createObjectURL(file);
-    setVideoSrc(videoUrl);
+    setVideoFile(file);
     setVideoUploaded(true);
   };
 
   const convertToGif = () => {
-    // Lógica para converter o vídeo em GIF
-    // Aqui você pode implementar a lógica para converter o vídeo em GIF
-    // por exemplo, usando uma biblioteca de conversão de vídeo para GIF
-    setShowAlert(true);
+    gifshot.createGIF(
+      {
+        video: [videoFile],
+        numFrames: 10,
+        frameDuration: 1
+      },
+      (obj) => {
+        if (!obj.error) {
+          const gifUrl = obj.image;
+          setGifUrl(gifUrl);
+        } else {
+          console.error('Erro ao converter o vídeo em GIF:', obj.error);
+        }
+      }
+    );
+  };
+
+  const handleDownload = () => {
+    if (gifUrl) {
+      const link = document.createElement('a');
+      link.href = gifUrl;
+      link.download = 'video.gif';
+      link.click();
+    }
   };
 
   return (
@@ -35,11 +55,17 @@ function App() {
         />
         {videoUploaded && (
           <div className="video-container">
-            <video id="uploaded-video" controls>
-              <source src={videoSrc} type="video/mp4" />
+            <video controls>
+              <source src={URL.createObjectURL(videoFile)} type="video/mp4" />
             </video>
             <button onClick={convertToGif}>Converter em GIF</button>
-            {showAlert && <p>Ok, botão clicado</p>}
+            {gifUrl && (
+              <div>
+                <a href={gifUrl} download="video.gif" onClick={handleDownload}>
+                  Baixar GIF
+                </a>
+              </div>
+            )}
           </div>
         )}
       </header>
